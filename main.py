38001109c0b3db4faa27a9f283aead89f338a105
Dishyma <marojas4126@gmail.com>
2022-09-09 10:08:24 +0000

    Add main.py and first UI

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+ventana = Tk()
+ventana.geometry()
+
+ventana.title("SuperMarkFa")
+label = Label(ventana, text = "SuperMarkFa").pack()
+
+
+ventana.mainloop()
