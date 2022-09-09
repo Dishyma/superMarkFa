128f81850bbf8076475b6fac150c660d69ce04fe
Mateo Rojas <69971210+Dishyma@users.noreply.github.com>
2022-09-09 11:41:55 -0500

    Add buttons on main window

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from tkinter import *
 from tkinter import ttk
+from turtle import left
+
 
 ventana = Tk()
-ventana.geometry()
+ventana.geometry("1280x720")
 
+helv36 = ("Chaparral Pro", 36, "bold")
 ventana.title("SuperMarkFa")
-label = Label(ventana, text = "SuperMarkFa").pack()
+label = Label(ventana, text = "SuperMarkFa", font=helv36, height=2).pack()
 
+separator = ttk.Separator(ventana, orient='horizontal')
+separator.pack(fill='x')
+
+btn1 = Button(ventana, text = 'boton 1', width=40, height=10).place(x=200 , y=150 )
+btn2 = Button(ventana, text = 'boton 2', width=40, height=10).place(x=800 , y=150 )
+btn3 = Button(ventana, text = 'boton 3', width=40, height=10).place(x=200 , y=450 )
+btn4 = Button(ventana, text = 'boton 4', width=40, height=10).place(x=800 , y=450 )
 
 ventana.mainloop()
