21a1b435da1d8b6a5dd57fbbce8b12b40cff7bf7
Dishyma <marojas4126@gmail.com>
2022-09-11 11:37:44 +0000

    Add login and register features

--- main.py
+++ main.py
@@ -1,11 +1,131 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 
+
+def atras(ventanaCerrar):
+    ventanaCerrar.destroy()
+    ventana.deiconify()
+
+def salir(ventanaSalir):
+    ventanaSalir.destroy()
+
+def validarDatos(txtUsuario, txtConstrasena):
+    usuario = txtUsuario
+    contrasena = txtConstrasena
+    if usuario == "":
+        messagebox. showerror(title="Campos Obligatorios", message="Ingrese el usuario")
+    elif contrasena == "":
+        messagebox. showerror(title="Campos Obligatorios", message="Ingrese la contraseña")
+    else:
+        messagebox.showinfo(title="Excelente", message="Validando datos")
+
+def comprar():
+    ventanaCompra = Toplevel()
+    ventanaCompra.geometry("1280x720")
+    ventanaCompra.title("Comprando")
+
+    lblCompra = Label(ventanaCompra, text="Añade los productos", font="times 36 bold", height=2).pack()
+
+    separator2 = ttk.Separator(ventanaCompra, orient='horizontal')
+    separator2.pack(fill='x')
+
+def login():
+    ventanaLogin = Toplevel()
+    ventanaLogin.geometry("800x600")
+    ventanaLogin.title("Iniciar Sesion")
+    
+    ventana.withdraw()
+    
+    logo = PhotoImage(file = "img/supermarket.png").subsample(6)
+    lblLogo = Label(ventanaLogin, image=logo)
+    lblLogo.image = logo
+    lblLogo.pack()
+
+    lblLogin = Label(ventanaLogin, text="Iniciar Sesion", font="times 36 bold", height=2)
+    lblLogin.pack()
+    
+    separator2 = ttk.Separator(ventanaLogin, orient='horizontal')
+    separator2.pack(fill=X)
+
+    lblUsuario = ttk.Label(ventanaLogin, text="Nombre de Usuario")
+    lblUsuario.pack()
+
+    usuario = StringVar()
+    usuario.set("Mateo")
+    txtUsuario = ttk.Entry(ventanaLogin, textvariable=usuario, width=30)
+    txtUsuario.pack()
+
+    lblcontrasena = ttk.Label(ventanaLogin, text="Contraseña")
+    lblcontrasena.pack()
+    contrasena = StringVar()
+    txtcontrasena = ttk.Entry(ventanaLogin, textvariable=contrasena, show="*", width=30)
+    txtcontrasena.pack()
+    
+    logoEnter = PhotoImage(file= "img/enter.png").subsample(7,7)
+    btn1 = Button(ventanaLogin, 
+                        text = ' Iniciar Sesion',
+                        font="times 11",  
+                        borderwidth=1, 
+                        relief="raised",
+                        image=logoEnter, 
+                        compound="left",
+                        command= lambda: validarDatos(txtUsuario.get(), txtcontrasena.get())
+                        )
+    btn1.image_names= logoEnter
+    btn1.pack(pady=20)
+
+    btn2 = Button(ventanaLogin, 
+                        text = 'Registrarme',
+                        font = "times 11",  
+                        borderwidth = 1, 
+                        relief ="raised",
+                        command = lambda : [salir(ventanaLogin), registrar()]
+                        )
+    btn2.pack(pady=20)
+
+    btn3 = Button(ventanaLogin, 
+                        text = 'Atras',
+                        font="times 11",
+                        borderwidth=1, 
+                        relief="raised",
+                        command=lambda: atras(ventanaLogin)
+                        )
+    btn3.pack(pady=20)
+
+def cupones():
+    ventanaCupones = Toplevel()
+    ventanaCupones.geometry("1280x720")
+    ventanaCupones.title("Iniciar Sesion")
+
+    lblCupones = Label(ventanaCupones, text="Cupones", font="times 36 bold", height=2).pack()
+
+    separator2 = ttk.Separator(ventanaCupones, orient='horizontal')
+    separator2.pack(fill='x')
+
+def registrar():
+    ventanaRegistrar = Toplevel()
+    ventanaRegistrar.geometry("1280x720")
+    ventanaRegistrar.title("Iniciar Sesion")
+
+    lblRegistrar = Label(ventanaRegistrar, text="Registrar", font="times 36 bold", height=2).pack()
+
+    separator2 = ttk.Separator(ventanaRegistrar, orient='horizontal')
+    separator2.pack(fill='x')
+
+    btn3 = Button(ventanaRegistrar, 
+                        text = 'Atras',
+                        font="times 11",
+                        borderwidth=1, 
+                        relief="raised",
+                        command=lambda: atras(ventanaRegistrar)
+                        )
+    btn3.pack(pady=20)
 
 ventana = Tk()
 ventana.geometry("1280x720")
-
 ventana.title("SuperMarkFa")
+
 label = Label(ventana, text = "SuperMarkFa", font="chaparral 36 italic", height=2).pack()
 
 separator = ttk.Separator(ventana, orient='horizontal')
@@ -23,7 +143,8 @@ btn1 = Button(ventana, text = 'Comprar',
                     borderwidth=1, 
                     relief="raised", 
                     image=icon1, 
-                    compound="top").place(x=200 , y=150 )
+                    compound="top",
+                    command=comprar).place(x=200 , y=150 )
 
 btn2 = Button(ventana, text = 'Iniciar Sesion', 
                     font="times 24 bold", 
@@ -32,7 +153,8 @@ btn2 = Button(ventana, text = 'Iniciar Sesion',
                     borderwidth=1, 
                     relief="raised",
                     image=icon2, 
-                    compound="top").place(x=800 , y=150 )
+                    compound="top",
+                    command=login).place(x=800 , y=150 )
 
 btn3 = Button(ventana, text = 'Cupones', 
                     font="times 13 bold", 
@@ -41,7 +163,8 @@ btn3 = Button(ventana, text = 'Cupones',
                     borderwidth=1, 
                     relief="raised",
                     image=icon3, 
-                    compound="top").place(x=200 , y=450 )
+                    compound="top",
+                    command=cupones).place(x=200 , y=450 )
 
 btn4 = Button(ventana, text = 'Registrar', 
                     font="times 13 bold", 
@@ -50,6 +173,14 @@ btn4 = Button(ventana, text = 'Registrar',
                     borderwidth=1, 
                     relief="raised", 
                     image=icon4, 
-                    compound="top").place(x=800 , y=450 )
+                    compound="top",
+                    command=registrar).place(x=800 , y=450 )
+
+btn5 = Button(ventana, text = 'Salir', 
+                    font="times 13 bold",
+                    borderwidth=1, 
+                    relief="raised", 
+                    command=lambda: salir(ventana))
+btn5.pack(side="bottom")
 
 ventana.mainloop()
